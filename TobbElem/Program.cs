46b68e1c73a43687e5d7abfc5869fa5741b28663
Bálint Horváth <2021130@dszcberegszaszi.hu>
2024-09-11 09:50:36 +0200

    Elem

---
+++ TobbElem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Összeg = 0;
+            while(Összeg<100)
+            { 
+                Console.WriteLine("Számokat kérek, az összegük nem haladhatja meg a 100-at");
+                var szam = int.Parse(Console.ReadLine());
+                if(szam!=null)
+                {
+                Console.WriteLine("Megadott szám:" + szam.ToString());
+                Összeg += 100;
+                Console.WriteLine("Összeg:" + Összeg);
+                if(Összeg >=100)
+                {
+                    Console.WriteLine("A megadott számok összege több, mint száz" + Összeg);
+                    Console.ReadKey();
+                    break;
+                }
+                
+                
+                
+                
+                }
+            }
+
+        }
+    }
+}
